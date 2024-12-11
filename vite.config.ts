2fada4da420eec34db62917c199152d66d783d4c
Swapnil Katwe <swapnil.katwe111@gmail.com>
2024-12-11 13:46:30 +0100

    Tailwind installed, vite config updated for folder path.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    // Added this configuration for getting path from @ to minimise folder path like ./../../xyz
+    alias: [{find: "@", replacement: path.resolve(__dirname, "src")}],
+  }
 })

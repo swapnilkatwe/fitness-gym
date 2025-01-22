979c39a4db037bd77470047c56382a27a97c0c93
Swapnil Katwe <swapnil.katwe111@gmail.com>
2025-01-22 13:16:56 +0100

    motion.div to use onViewPortEnter value updated for showing selected menu while scroll.

--- src/scenes/Home/index.tsx
+++ src/scenes/Home/index.tsx
@@ -21,20 +21,21 @@ const Home = ({ setSelectedPage }: Props) => {
             id="home"
             className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
             {/* IMAGE AND MAIN HEADER */}
-            <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
+            <motion.div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6"
+                onViewportEnter={() => setSelectedPage(SelectedPage.Home)}>
                 {/* MAIN HEADER */}
                 <div className="z-10 mt-32 md:basis-3/5">
                     {/* HEADINGS */}
-                    <motion.div 
-                    className="md:-mt-20"
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, amount: 0.5}}
-                    transition={{duration: 0.5}}
-                    variants={{
-                        hidden: {opacity: 0, x: -50},
-                        visible: {opacity: 1, x: 0}
-                    }}>
+                    <motion.div
+                        className="md:-mt-20"
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, x: -50 },
+                            visible: { opacity: 1, x: 0 }
+                        }}>
                         {/*  */}
                         <div className="relative">
                             <div className="before:absolute before:-top-20 before:-left-20 md:before:content-evolvetext before:z-[-1]">
@@ -47,14 +48,14 @@ const Home = ({ setSelectedPage }: Props) => {
                     </motion.div>
                     {/* ACTIONS */}
                     <motion.div className="mt-8 flex items-center gap-8"
-                      initial="hidden"
-                      whileInView="visible"
-                      viewport={{ once: true, amount: 0.5}}
-                      transition={{delay: 0.2, duration: 0.5}}
-                      variants={{
-                          hidden: {opacity: 0, x: -50},
-                          visible: {opacity: 1, x: 0}
-                      }}>
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ delay: 0.2, duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, x: -50 },
+                            visible: { opacity: 1, x: 0 }
+                        }}>
                         <ActionButton setSelectedPage={setSelectedPage}>
                             Join Now
                         </ActionButton>
@@ -70,7 +71,7 @@ const Home = ({ setSelectedPage }: Props) => {
                 <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
                     <img src={HomePageGraphic} alt="home-page-graphic" />
                 </div>
-            </div>
+            </motion.div>
             {/* SPONSERS */}
             {isAboveMediumScreen && (
                 <div className="h-[150] w-full bg-orange-100 py-10">
